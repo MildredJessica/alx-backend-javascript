19ac4c817b995f0a81f04df9ca10d3d3c487d848
MildredJessica <nwonyejessica@gmail.com>
2023-10-08 22:57:37 +0000

    Added courses, building, pricing and currency classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,25 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nom) {
+    this._name = nom;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(cod) {
+    this._name = cod;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
